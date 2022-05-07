69e15853cfc32f2ff967028be526b6e99dbdfe52
Maxi Olea <maxibsolea@gmail.com>
2022-05-07 19:07:12 -0300

    Componentes Agregados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SidemenuComponent } from './components/sidemenu/sidemenu.component';
+import { ContentComponent } from './components/content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidemenuComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule
